cb8cc13cd3174d0a01ae049daa4e11cc2fd0308a
EhsonjonKomiljonov <ekomiljonov29@gmail.com>
2023-08-29 15:26:24 +0500

    add user profile

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ export const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static(process.cwd() + '/public'))
 app.set('view engine', 'ejs');
 
 app.use('/users', userRouter);
--- src/middlewares/user.check.js
+++ src/middlewares/user.check.js
@@ -10,9 +10,35 @@ export const userCheck = (req, res, next) => {
       district: Joi.string().required().min(3).max(50),
       address: Joi.string().required().min(2).max(50),
       password: Joi.string()
+        .required()
         .min(6)
         .max(60)
         .pattern(new RegExp('^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])')),
+    });
+
+    const checkUser = validateUser.validate(req.body);
+
+    if (checkUser?.error) throw new Error(checkUser.error);
+
+    next();
+  } catch (err) {
+    return res.send({
+      status: 501,
+      message: err.message,
+      data: null,
+    });
+  }
+};
+
+export const userEditCheck = (req, res, next) => {
+  try {
+    const validateUser = Joi.object({
+      first_name: Joi.string().min(3).max(60),
+      last_name: Joi.string().min(3).max(60),
+      email: Joi.string().email().min(6).max(150),
+      region: Joi.string().min(3).max(50),
+      district: Joi.string().min(3).max(50),
+      address: Joi.string().min(2).max(50),
     });
 
     const checkUser = validateUser.validate(req.body);
