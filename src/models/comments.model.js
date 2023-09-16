24843e87adf6464e6be80061f8c167254f0e30f6
EhsonjonKomiljonov <ekomiljonov29@gmail.com>
2023-09-16 18:32:54 +0500

    edit

--- src/models/comments.model.js
+++ src/models/comments.model.js
@@ -1,23 +1,31 @@
 import { Schema, Types, model } from 'mongoose';
 
-const comment = new Schema({
-  text: {
-    type: String,
-    required: true,
+const comment = new Schema(
+  {
+    text: {
+      type: String,
+      required: true,
+    },
+    user_ref_id: {
+      type: Types.ObjectId,
+      ref: 'users',
+      key: '_id',
+      required: true,
+    },
+    product_ref_id: {
+      type: Types.ObjectId,
+      ref: 'buyerPosts',
+      ref: 'sellerPosts',
+      key: '_id',
+      required: true,
+    },
   },
-  user_ref_id: {
-    type: Types.ObjectId,
-    ref: 'users',
-    key: '_id',
-    required: true,
-  },
-  product_ref_id: {
-    type: Types.ObjectId,
-    ref: 'buyerPosts',
-    ref: 'sellerPosts',
-    key: '_id',
-    required: true,
-  },
-});
+  {
+    timestamps: {
+      createdAt: 'created_at',
+      updatedAt: false,
+    },
+  }
+);
 
 export const commentModel = model('comments', comment);
